28cd28c4a1480bbd6ae6049375887cf0ad50f881
hamidme <hwaasi@gmail.com>
2022-12-19 07:41:01 +0400

    part04_20 modified

--- part04-Part04_20.Books/src/main/java/Book.java
+++ part04-Part04_20.Books/src/main/java/Book.java
@@ -9,32 +9,60 @@
  * @author HAMIDME
  */
 public class Book {
+
     private String title;
     private int pages;
     private int publicationYear;
     
-    public Book (String title, int pages, int publicationYear){
+    
+
+//    public Book(String title, int pages, int publicationYear) {
+//        this.title = title;
+//        this.pages = pages;
+//        this.publicationYear = publicationYear;
+//
+//    }
+
+//    public String getTitle(){
+//        return this.title;
+//    }
+//    
+//    public int getPage(){
+//        return this.pages;
+//    }
+//    
+//    public int getPublicationYear(){
+//        return this.publicationYear;
+//    }
+//    
+//    @Override
+//    public String toString(){
+//        return this.title + ", " + this.pages + " pages, " + this.publicationYear;
+//    }
+
+    public Book(String title, int pages, int publicationYear) {
         this.title = title;
         this.pages = pages;
         this.publicationYear = publicationYear;
-        
     }
-    
-    public String getTitle(){
-        return this.title;
+
+    public String getTitle() {
+        return title;
     }
-    
-    public int getPage(){
-        return this.pages;
+
+    public int getPages() {
+        return pages;
     }
-    
-    public int getPublicationYear(){
-        return this.publicationYear;
+
+    public int getPublicationYear() {
+        return publicationYear;
     }
-    
+
     @Override
-    public String toString(){
-        return this.title + ", " + this.pages + " pages, " + this.publicationYear;
+    public String toString() {
+        return title + ", "+ pages + " pages, " + publicationYear;
     }
+    
+    
     
 }
